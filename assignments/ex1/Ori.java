932054737598895862c54f275382374bca3e382a
oriamit12345 <oriamit12345@gmail.com>
2024-12-09 21:00:37 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ori.java
+++ assignments/ex1/Ori.java
@@ -39,5 +39,6 @@ public class Ori {
                 }
             }
         }
+        return false;
     }
 }
--- assignments/ex1/OriTest.java
+++ assignments/ex1/OriTest.java
@@ -16,8 +16,27 @@ public class OriTest {
         }
         String[] NOT_GOOD = {"542ih", "00/124", "  12120"};
         for (int i = 0; i < GOOD.length; i = i + 1) {
-            boolean ok = Ori.isNumber(NOT_GOOD[i]);
-            assertFalse(ok);
+            boolean not_ok = Ori.isNumber(NOT_GOOD[i]);
+            assertFalse(not_ok);
         }
     }
+
+    @Test
+    void Number_with_base() {
+        String[] GOOD = {"1b2", "1b2", "01b2", "123bA", "ABbG", "0bA"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean ok = Ori.Number_with_base(GOOD[i]);
+            assertTrue(ok);
+        }
+
+        String[] NOT_GOOD = {"b2", "2b2"};
+        for (int i = 0; i < NOT_GOOD.length; i = i + 1) {
+            boolean not_ok = Ori.Number_with_base(NOT_GOOD[i]);
+            assertFalse(not_ok);
+        }
+
+
+    }
 }
+
+
