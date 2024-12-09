592d526660789308f3834f1567a6a71a28b40d9a
oriamit12345 <oriamit12345@gmail.com>
2024-12-09 20:07:37 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ori.java
+++ assignments/ex1/Ori.java
@@ -2,33 +2,42 @@ package assignments.ex1;
 
 public class Ori {
 
-    public static void main(String[] args) {
+    public static boolean isNumber(String MYnumber) {
+        if (All_number(MYnumber)) {
+            return true;
+        }
+        if (Number_with_base(MYnumber)){
+            return true;
+        }
+        return false;
     }
 
+    public static boolean All_number(String MYnumber) {
+        int siz = MYnumber.length() - 1;
+        for (int i = 0; i <= siz; i = i + 1) {
+            if ('0' <= MYnumber.charAt(i) && MYnumber.charAt(i) <= '9') {
+                if (i == siz) {
+                    return true;
+                }
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
 
-    public static boolean isNumber(String MYnumber) {
+    public static boolean Number_with_base(String MYnumber) {
 
         int siz = MYnumber.length() - 1;
         char Base = MYnumber.charAt(siz);
-
-
-        for (int i = 0 ; i <= siz ; i = i + 1 ){
-
-            if( ('0' < MYnumber.charAt(i) && MYnumber.charAt(i) <'9') && i == siz ){
-                return true;
-            }
-        }
-
         char Limit = MYnumber.charAt(siz - 1);
-        if (Limit == 'b' && ( '1' < Base && Base <= '9' || 'A' <= Base && Base <= 'G' ) ){
-            for (int i = 0 ; i < (siz - 1) ; i = i + 1){
+        if (Limit == 'b' && ('1' < Base && Base <= '9' || 'A' <= Base && Base <= 'G')) {
+            for (int i = 0; i < (siz - 1); i = i + 1) {
                 char Key = MYnumber.charAt(i);
-                if (('0' <= Key && Key <='9' || 'A' <= Key && Key <= 'G' ) && Key < Base ){
-                    return true ;
+                if (('0' <= Key && Key <= '9' || 'A' <= Key && Key <= 'G') && Key < Base) {
+                    return true;
                 }
             }
-
         }
-        return false ;
     }
 }
--- assignments/ex1/OriTest.java
+++ assignments/ex1/OriTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OriTest {
     @Test
-void isNumber () {
-        String GOOD = "1";
-        boolean ok = Ori.isNumber(GOOD);
-        assertTrue(ok);
+    void Allnumber() {
+
+        String[] GOOD = {"1258487", "000111", "123000987"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean ok = Ori.isNumber(GOOD[i]);
+            assertTrue(ok);
+        }
+        String[] NOT_GOOD = {"542ih", "00/124", "  12120"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean ok = Ori.isNumber(NOT_GOOD[i]);
+            assertFalse(ok);
+        }
     }
 }
