5bb91bab96bc6914790fdab3eca5715f4588caf0
oriamit12345 <oriamit12345@gmail.com>
2024-12-09 10:34:35 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ex1.java
+++ assignments/ex1/Ex1.java
@@ -30,12 +30,28 @@ public class Ex1 {
      * @param a a String representing a number
      * @return true iff the given String is in a number format
      */
-    public static boolean isNumber(String a) {
-        boolean ans = true;
-        // add your code here
+    public static boolean isNumber(String MYnumber) {
 
-        ////////////////////
-        return ans;
+        int siz = MYnumber.length() - 1;
+        char Base = MYnumber.charAt(siz);
+        char Limit = MYnumber.charAt(siz - 1);
+
+        for (int i = 0 ; i < siz ; i = i + 1  ){
+            if( '0' < MYnumber.charAt(i) && MYnumber.charAt(i) <'9' ){
+                return true;
+            }
+        }
+
+        if (Limit == 'b' && ( '1' < Base && Base <= '9' || 'A' <= Base && Base <= 'G' ) ){
+            for (int i = 0 ; i < (siz - 1) ; i = i + 1){
+                char Key = MYnumber.charAt(i);
+                if (('0' <= Key && Key <='9' || 'A' <= Key && Key <= 'G' ) && Key < Base ){
+                    return true ;
+                }
+            }
+
+        }
+        return false ;
     }
 
     /**
