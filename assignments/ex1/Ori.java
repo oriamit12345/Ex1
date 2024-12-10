04b5d42581cf86fec0c2024396896dd1d4def76e
oriamit12345 <oriamit12345@gmail.com>
2024-12-10 17:24:29 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ori.java
+++ assignments/ex1/Ori.java
@@ -20,9 +20,6 @@ public class Ori {
                     return true;
                 }
             }
-            else {
-                break;
-            }
         }
         return false;
     }
@@ -32,31 +29,35 @@ public class Ori {
         if (siz > 2) {
             char base = MYnumber.charAt(siz - 1);
             char limit = MYnumber.charAt(siz - 2);
-            if ((limit == 'b') && (('1' <base && base <= '9') || ('A' <= base && base <= 'G'))) {
+            int sum = 0;
+            if ((limit == 'b') && (('1' < base && base <= '9')  || ('A' <= base && base <= 'G'))) {
                 for (int i = 0; i < siz - 2; i = i + 1) {
                     char Key = MYnumber.charAt(i);
                     if (('0' <= Key && Key <= '9' || 'A' <= Key && Key < 'G') && (Key < base)) {
-
+                        sum = sum + 1;
                     }
                 }
+            }
+            if (sum == siz -2){
+                return true;
             }
             return false;
         }
         return false;
     }
-   /* public static int number2Int(String MYnumber1) {
-        int ans = -1;
-        int siz = MYnumber1.length();
-        String s1 = MYnumber1.substring(0,siz -2);
-        String s2 =
+   public static int number2Int(String MYnumber1) {
+        if(All_number(MYnumber1) ){
+            int decimal_value = Integer.parseInt(s1_num,'A');
 
+        }
+
+        int siz = MYnumber1.length();
+        String s1_num = MYnumber1.substring(0,siz -2);
+        char s2_base = MYnumber1.charAt(siz - 1);
+        int decimal_value = Integer.parseInt(s1_num,s2_base);
 
         return ans;
     }
-
-
-    Character.getNumericValue();
-
-    */
+    int ans = -1;
 }
 
