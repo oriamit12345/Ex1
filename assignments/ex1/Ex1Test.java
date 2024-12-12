59a08f26fa9db1e419dfaf5b18c45c51b6e4334f
oriamit12345 <oriamit12345@gmail.com>
2024-12-12 15:04:44 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ex1Test.java
+++ assignments/ex1/Ex1Test.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Make sure you complete all the needed JUnits
  */
 public class Ex1Test {
+
+
     @Test
     void computeNumberTest() {
         String s2 = "1011b2";
@@ -20,6 +22,7 @@ public class Ex1Test {
         assertEquals(v,v2);
         assertTrue(Ex1.equals(s10,s2));
     }
+
 
     @Test
     void isBasisNumberTest() {
@@ -34,14 +37,89 @@ public class Ex1Test {
             assertFalse(not_ok);
         }
     }
+
+
     @Test
-    void int2NumberTest() {
-        // implement this test
+    void int2Number_test() {
+        int[] i1_number_arr ={728,728,42,100,2020,256,17};
+        int[] i1_base_arr ={16,4,2,2,16,8,17};
+        String[] s1_true_arr = {"2D8","23120", "101010", "1100100", "7E4","400","ERR: wrong base, should be [2,16], got (17)"};
+
+        for(int i = 0; i < i1_number_arr.length; i = i + 1){
+            int in1_base = i1_base_arr [i] ;
+            int in1_number = i1_number_arr [i] ;
+            String s1_result_str = Ori.int2Number(in1_number, in1_base) ;
+            String s1_true = s1_true_arr [i] ;
+            assertEquals(s1_true, s1_result_str, "numbers not match! expected value was: " + s1_true + " received: " + s1_result_str);
+        }
     }
+
+
     @Test
     void maxIndexTest() {
         // implement this test
     }
 
+
+    @Test
+    void All_number_test() {
+
+        String[] GOOD = {"1258487", "000111", "123000987"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean ok = Ori.All_number(GOOD[i]);
+            assertTrue(ok);
+        }
+        String[] NOT_GOOD = {"542ih", "00/124", "12120"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean not_ok = Ori.All_number(NOT_GOOD[i]);
+            assertFalse(not_ok);
+        }
+    }
+
+
+    @Test
+    void Number_with_base_test() {
+
+        String[] GOOD = {"1b2", "01b2", "123bA", "ABbG", "0bA"};
+        for (int i = 0; i < GOOD.length; i = i + 1) {
+            boolean ok = Ori.Number_with_base(GOOD[i]);
+            assertTrue(ok);
+        }
+        String[] NOT_GOOD = {"b2", "2b2", "1G3bG", " BbG", "0bbA", "abB", "!@b2", "A", "1bb2"};
+        for (int i = 0; i < NOT_GOOD.length; i = i + 1) {
+            boolean not_ok = Ori.Number_with_base(NOT_GOOD[i]);
+            assertFalse(not_ok);
+        }
+
+
+        @Test
+        void number2Int_test() {
+            String[] s1_arr = {"1b2","01b2", "123bA", "ABbG", "0bA", "589","58BA"};
+            int[] s1_true_arr ={1,1,123,171,0,589,-1};
+
+            for(int i = 0; i < s1_arr.length; i = i + 1){
+                String s1 = s1_arr [i] ;
+                int s1_result_int = Ori.number2Int(s1);
+                int s1_true = s1_true_arr [i] ;
+                assertEquals(s1_true, s1_result_int, "numbers not match! expected value was: " + s1_true + " received: " + s1_result_int);
+            }
+        }
+    }
+
+
+    @Test
+    void int2Number_test() {
+        int[] i1_number_arr ={728,728,42,100,2020,256,17};
+        int[] i1_base_arr ={16,4,2,2,16,8,17};
+        String[] s1_true_arr = {"2D8","23120", "101010", "1100100", "7E4","400","ERR: wrong base, should be [2,16], got (17)"};
+
+        for(int i = 0; i < i1_number_arr.length; i = i + 1){
+            int in1_base = i1_base_arr [i] ;
+            int in1_number = i1_number_arr [i] ;
+            String s1_result_str = Ori.int2Number(in1_number, in1_base) ;
+            String s1_true = s1_true_arr [i] ;
+            assertEquals(s1_true, s1_result_str, "numbers not match! expected value was: " + s1_true + " received: " + s1_result_str);
+        }
+    }
     // Add additional test functions - test as much as you can.
 }
--- assignments/ex1/Ori.java
+++ assignments/ex1/Ori.java
@@ -1,45 +1,12 @@
 package assignments.ex1;
 
-public class Ori {
+public class Ori {}
 
 
 
 
 
 
-    public static String int2Number(int num, int base) {
-        if ((1 < base && base <= 16 )) {
-            int result = num;
-            int remainder;
-            int counter = 0;
-            char[] waste_collection = new char[32];
-            while (result != 0) {
-                remainder = result % base;
-                if ( remainder < 10) {
-                    remainder = remainder + 48;
-                }
-                else {
-                    remainder = remainder + 55;
-                }
-                char ch_remainder = (char) remainder ;
-                waste_collection [counter] = ch_remainder ;
-                result = result / base;
-                counter = counter + 1;
-            }
-
-            char[] good_collection = new char[counter] ;
-            int loca_go = counter - 1 ;
-            for( int loca_wa = 0 ; loca_wa < counter ; loca_wa = loca_wa + 1 ){
-                good_collection[loca_go] = waste_collection [loca_wa] ;
-                loca_go = loca_go - 1 ;
-            }
-            String num_in_base = new String(good_collection);
-            return num_in_base ;
-        }
-         String st_base =  Integer.toString(base); ;
-        return "ERR: wrong base, should be [2,16], got (" + st_base + ')' ;
-    }
-}
 
 
 
