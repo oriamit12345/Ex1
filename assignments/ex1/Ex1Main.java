9721b6ddd75475c4b370ec8a1285f600536cceda
oriamit12345 <oriamit12345@gmail.com>
2024-12-13 07:09:47 +0200

    Merge remote-tracking branch 'origin/main'

--- assignments/ex1/Ex1Main.java
+++ assignments/ex1/Ex1Main.java
@@ -10,26 +10,32 @@ public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String num1 = "", num2 = "", quit = "quit";
+
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
+
             if (!num1.equals("quit")) {
+
                 if (Ex1.isNumber(num1)) {
                     int int_num1 = Ex1.number2Int(num1);
                     System.out.println("num1 = " + num1 + " is number: true , value: " + int_num1);
-
                     System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
                     num2 = sc.next();
+
                     if (!num2.equals("quit")) {
+
                         if (Ex1.isNumber(num2)) {
                             int int_num2 = Ex1.number2Int(num2);
+
                             if (Ex1.isNumber(num2)) {
                                 System.out.println("num2 = " + num2 + " is number: true , value: " + int_num2);
                                 System.out.println("Enter a base for output: (a number [2,16])");
                                 Scanner in = new Scanner(System.in);
                                 int base = in.nextInt();
+
                                 if ((1 < base && base <= 16 )) {
                                     int result_sum = int_num2 + int_num1;
                                     int result_multi = int_num2 * int_num1;
@@ -37,7 +43,6 @@ public class Ex1Main {
                                     String st_multi = Ex1.int2Number(result_multi, base);
                                     System.out.println(num1 + " + " + num2 + " = " + st_sum);
                                     System.out.println(num1 + " * " + num2 + " = " + st_multi);
-
                                     String[] st_arr = {num1, num2, st_sum, st_multi};
                                     int int_max_arr = Ex1.maxIndex(st_arr);
                                     String st_max_arr = Ex1.int2Number(int_max_arr, base);
--- assignments/ex1/README.md
+++ assignments/ex1/README.md
@@ -1,12 +1,18 @@
 # Ex1
-his is my first Java assignment
-for the Intro to Computer Science course.
-The project creates a simple tool that helps convert numbers
-between different bases (2-16) and do basic math operations.
-I wrote functions to check if numbers are valid,
-add and multiply them, and find the biggest number in a set.
-The program runs in the console and lets you input numbers,
-convert them, and see results.
+This is my first Java assignment in github
+for the Intro to Introduction to calculus course.
 
-"I also wrote JUnit tests that ensure the correctness of our number
-conversion methods by testing different tape and conversion scenarios."
+The project creates a simple tool
+that helps convert numbers between different bases (2-16)
+and do basic math operations.
+
+I wrote functions to check if numbers are valid,
+add and multiply them,
+and find the biggest number in a set.
+
+The program runs in the console
+and lets you input numbers, convert them, and see results.
+
+I also wrote JUnit tests
+that ensure the correctness of our number conversion methods
+by testing different tape and conversion scenarios.
